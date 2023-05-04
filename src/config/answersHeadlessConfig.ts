3bb1ec07f13d2ca3355172d5cf1d29726d63c215
ds-mansi <mansi.gurnani@dotsquares.com>
2023-05-04 15:01:44 +0530

    first commit

---
+++ src/config/answersHeadlessConfig.ts
@@ -0,0 +1,20 @@
+
+export const AnswerExperienceConfig = {
+  limit:3,
+  locale: "en",
+  apiKey : "99f3ecaea33bf69336f8a205da5d59a4",
+  verticalKey : "locations",
+  experienceKey : "beansuntory",
+  experienceVersion: "STAGING",
+  locationRadius: 804672,
+  sessionTrackingEnabled: true,
+  endpoints: {
+    universalSearch: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/query",
+    verticalSearch: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/query",
+    questionSubmission: "https://liveapi-sandbox.yext.com/v2/accounts/me/createQuestion",
+    universalAutocomplete: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/autocomplete",
+    verticalAutocomplete: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/autocomplete",
+    filterSearch: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/filtersearch",
+
+  }
+}
